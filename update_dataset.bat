3bd51e6d93839ac93eaa18237c2931162e2cdc66
alisaleks <alisa.aleksanyan@alumni.esade.edu>
2024-09-08 22:00:32 +0200

    Automatic dataset update

--- update_dataset.bat
+++ update_dataset.bat
@@ -2,14 +2,18 @@
 echo Changing to app directory
 cd "C:\Users\aaleksan\OneDrive - Amplifon S.p.A\Documentos\python_alisa\saturation\Saturation\Satapp\agenda_app" || (echo Failed to change directory & exit /b 1)
 
-echo Adding all modified files to Git
-git add . || (echo Failed to add files & exit /b 1)
+echo Adding files to Git
+git add . || (echo Failed to add shiftslots.xlsx & exit /b 1)
 
 echo Committing changes
 git commit -m "Automatic dataset update" || (echo Nothing to commit or commit failed & exit /b 1)
+
+echo Pulling latest changes from remote
+git pull origin main --rebase || (echo Failed to pull changes & exit /b 1)
 
 echo Pushing to GitHub
 git push origin main || (echo Push failed & exit /b 1)
 
 echo Push completed successfully
-pause  # This will keep the terminal open so you can read the output
+pause
+exit /b 0
